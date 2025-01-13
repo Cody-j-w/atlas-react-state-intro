66c8990377e554e5ee6611f56a15cc821b3d299a
Cody Walenciak <codywalenciak@gmail.com>
2025-01-13 15:03:29 -0600

    Wrap app in context manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
+import {EnrollmentContextProvider} from './EnrollmentContext';
+import { useState } from "react";
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <SchoolCatalog />
-      <ClassSchedule />
+      <EnrollmentContextProvider>
+        <Header />
+        <SchoolCatalog />
+        <ClassSchedule />
+      </EnrollmentContextProvider>
     </div>
   );
 }
